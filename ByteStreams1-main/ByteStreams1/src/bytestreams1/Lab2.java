153113e9c2a1d564902664be120c5092945ef6db
Chanapon <111365381+ChanaponA@users.noreply.github.com>
2023-04-25 11:17:29 +0700

    Add files via upload

---
+++ ByteStreams1-main/ByteStreams1/src/bytestreams1/Lab2.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bytestreams1;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author potae
+ */
+public class Lab2 {
+
+    public static void main(String[] args) {
+        String s = "Hello World!";
+        byte[] b = s.getBytes();
+        try {
+            FileOutputStream output = new FileOutputStream("data.dat");
+            output.write(b);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Lab2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Lab2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            FileInputStream input = new FileInputStream("data.dat");
+            int ch;
+            while ((ch = input.read()) != -1) {
+                System.out.print((char) ch);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Lab2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Lab2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
