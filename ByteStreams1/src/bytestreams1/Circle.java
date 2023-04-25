ded7f63cf1c51fa0d66f6bdd4bde8ea915ff3714
Chanapon <111365381+ChanaponA@users.noreply.github.com>
2023-04-25 11:26:50 +0700

    Add files via upload

---
+++ ByteStreams1/src/bytestreams1/Circle.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bytestreams1;
+
+/**
+ *
+ * @author potae
+ */
+public class Circle implements Shape{
+    @Override
+    public void draw(){
+        System.out.println("Shape: Circle");
+    
+    }
+    
+}
